4d9df2eac067c65286393f4875951a9e42ce756d
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2019-10-06 20:09:15 +0600

    Add Bangla in utils.py

--- newspaper/utils.py
+++ newspaper/utils.py
@@ -358,6 +358,7 @@ def print_available_languages():
         'ar': 'Arabic',
         'be': 'Belarusian',
         'bg': 'Bulgarian',
+        'bn': 'Bangla',
         'da': 'Danish',
         'de': 'German',
         'el': 'Greek',
